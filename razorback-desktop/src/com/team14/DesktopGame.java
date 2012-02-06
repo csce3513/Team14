b00786ab1b065ba9420d8bc424f7caad228c3429
Nathan Hyatt <nhyatt@gmail.com>
2012-02-05 22:45:11 -0600

    Initial project setup for Eclipse

---
+++ razorback-desktop/src/com/team14/DesktopGame.java
@@ -0,0 +1,9 @@
+package com.team14;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class DesktopGame {
+        public static void main (String[] args) {
+                new LwjglApplication(new Game(), "Game", 800, 600, false);
+        }
+}
