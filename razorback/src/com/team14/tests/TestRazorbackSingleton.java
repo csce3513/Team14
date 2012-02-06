471c6330723a9ac5b670136c2de43b423e1f3775
Nathan Hyatt <nhyatt@gmail.com>
2012-02-05 23:10:24 -0600

    junit.jar -> libs, updated project  class path, stub for first test

---
+++ razorback/src/com/team14/tests/TestRazorbackSingleton.java
@@ -0,0 +1,14 @@
+package com.team14.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRazorbackSingleton {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
