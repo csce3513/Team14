e686ad3b151c729dfa6c3eaa2dc5c95199f1fbd0
Jeremy Barr <jxb040@uark.edu>
2012-02-10 14:11:49 -0600

    Testing for Input

--- razorback/src/com/team14/tests/TestInput.java
+++ razorback/src/com/team14/tests/TestInput.java
@@ -1,15 +1,22 @@
 package com.team14.tests;
 
 import static org.junit.Assert.*;
-
+import java.io.*;
 import org.junit.Test;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 
 public class TestInput {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		byte [] buff ={62};
+		BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(buff));
+		System.setIn(in);
+		boolean b=true;
+		//b=Gdx.input.isKeyPressed(Keys.SPACE);
+		assertTrue(b);
 	}
 
 }
