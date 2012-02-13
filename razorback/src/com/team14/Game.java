1cdc5328ee31f5233ea77801e88636ef98f6cac3
Nathan Hyatt <nhyatt@gmail.com>
2012-02-13 01:13:49 -0600

    Created test tile map, started map rendering implementation

--- razorback/src/com/team14/Game.java
+++ razorback/src/com/team14/Game.java
@@ -1,10 +1,24 @@
 package com.team14;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.g2d.tiled.TileMapRenderer;
+import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
+import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
+import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class Game implements ApplicationListener {
-
+	OrthographicCamera cam = new OrthographicCamera();
+	Vector3 tmp = new Vector3();
+    TileMapRenderer tileMapRenderer;
+    SpriteBatch spriteBatch;
+    
 	private boolean gameOver = false;
+	boolean automove = true;
 	
 	public void setGameOver(boolean status)
 	{
@@ -19,7 +33,19 @@ public class Game implements ApplicationListener {
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
+//        TileMapRenderer tileMapRenderer;
+        TiledMap map;
+        TileAtlas atlas;
+        // TiledLoader loader;
+        spriteBatch = new SpriteBatch();
+        // Load the tmx file into map
+        map = TiledLoader.createMap(Gdx.files.internal("assets/tiles/testmap.tmx"));
 
+        // Load the tiles into atlas
+        atlas = new TileAtlas(map, Gdx.files.internal("assets/tiles/"));
+
+        // Create the renderer
+        tileMapRenderer = new TileMapRenderer(map, atlas, 32, 32, 5, 5);
 	}
 
 	@Override
@@ -36,6 +62,28 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void render() {
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        // if (automove) {
+        //     updateCameraPosition();
+        // }
+
+        cam.zoom = 0.9f;
+        cam.update();
+        
+        tileMapRenderer.render(cam);
+
+        spriteBatch.begin();
+        //font.draw(spriteBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 20, 20);
+        //font.draw(spriteBatch, "InitialCol, LastCol: " + tileMapRenderer.getInitialCol() + "," + tileMapRenderer.getLastCol(), 20,
+         //       40);
+        //font.draw(spriteBatch, "InitialRow, LastRow: " + tileMapRenderer.getInitialRow() + "," + tileMapRenderer.getLastRow(), 20,
+         //       60);
+        
+        tmp.set(0, 0, 0);
+        cam.unproject(tmp);
+        //font.draw(spriteBatch, "Location: " + tmp.x + "," + tmp.y, 20, 80);
+        spriteBatch.end();
 		// TODO Auto-generated method stub
 
 	}
