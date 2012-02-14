c1307b998aa71ece1d207f8d62dbc36ea4c7820a
Nathan Hyatt <nhyatt@gmail.com>
2012-02-14 12:23:02 -0600

    ...

--- razorback/src/com/team14/Game.java
+++ razorback/src/com/team14/Game.java
@@ -19,6 +19,10 @@ package com.team14;
  * http://dpk.net/2011/05/08/libgdx-box2d-tiled-maps-full-working-example-part-2/
  */
 
+/**
+ * TODO:
+ * Make Razorback class extend Body class, for testing position setting 
+ */
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -188,6 +192,7 @@ public class Game implements ApplicationListener {
 		debugRenderer = new Box2DDebugRenderer();
 
 		lastRender = System.nanoTime();
+		//jumper.setLinearVelocity(new Vector2(100.0f, 1.0f));
 	}
 
 	@Override
@@ -248,7 +253,7 @@ public class Game implements ApplicationListener {
 		 * 
 		 * The impulses are applied to the center of the jumper.
 		 */
-		if (moveRight) {
+				if (moveRight) {
 			jumper.applyLinearImpulse(new Vector2(0.05f, 0.0f),
 					jumper.getWorldCenter());
 			if (jumperFacingRight == false) {
@@ -299,7 +304,7 @@ public class Game implements ApplicationListener {
 
 		tiledMapHelper.getCamera().position.x = PIXELS_PER_METER
 				* jumper.getPosition().x;
-
+		
 		/**
 		 * Ensure that the camera is only showing the map, nothing outside.
 		 */
@@ -331,8 +336,8 @@ public class Game implements ApplicationListener {
 		spriteBatch.begin();
 
 		jumperSprite.setPosition(
-				PIXELS_PER_METER * jumper.getPosition().x
-						- jumperSprite.getWidth() / 2,
+				PIXELS_PER_METER * jumper.getPosition().x + 10,
+				//		- jumperSprite.getWidth() / 2,
 				PIXELS_PER_METER * jumper.getPosition().y
 						- jumperSprite.getHeight() / 2);
 		jumperSprite.draw(spriteBatch);
@@ -375,101 +380,3 @@ public class Game implements ApplicationListener {
 	}
 }
 
-//package com.team14;
-//
-//import com.badlogic.gdx.ApplicationListener;
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.GL10;
-//import com.badlogic.gdx.graphics.g2d.tiled.TileMapRenderer;
-//import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
-//import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
-//import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
-//import com.badlogic.gdx.graphics.OrthographicCamera;
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-//import com.badlogic.gdx.math.Vector3;
-//
-//public class Game implements ApplicationListener {
-//	OrthographicCamera cam = new OrthographicCamera();
-//	Vector3 tmp = new Vector3();
-//    TileMapRenderer tileMapRenderer;
-//    SpriteBatch spriteBatch;
-//    
-//	private boolean gameOver = false;
-//	boolean automove = true;
-//	
-//	public void setGameOver(boolean status)
-//	{
-//		gameOver = status;
-//	}
-//	
-//	public boolean isGameOver()
-//	{
-//		return gameOver;
-//	}
-//	
-//	@Override
-//	public void create() {
-////        TileMapRenderer tileMapRenderer;
-//        TiledMap map;
-//        TileAtlas atlas;
-//        // TiledLoader loader;
-//        spriteBatch = new SpriteBatch();
-//        // Load the tmx file into map
-//        map = TiledLoader.createMap(Gdx.files.internal("assets/tiles/testmap.tmx"));
-//
-//        // Load the tiles into atlas
-//        atlas = new TileAtlas(map, Gdx.files.internal("assets/tiles/"));
-//
-//        // Create the renderer
-//        tileMapRenderer = new TileMapRenderer(map, atlas, 32, 32, 5, 5);
-//	}
-//
-//	@Override
-//	public void dispose() {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void pause() {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void render() {
-//        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-//
-//        // if (automove) {
-//        //     updateCameraPosition();
-//        // }
-//
-//        cam.zoom = 0.9f;
-//        cam.update();
-//        
-//        tileMapRenderer.render(cam);
-//
-//        spriteBatch.begin();
-//        //font.draw(spriteBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 20, 20);
-//        //font.draw(spriteBatch, "InitialCol, LastCol: " + tileMapRenderer.getInitialCol() + "," + tileMapRenderer.getLastCol(), 20,
-//         //       40);
-//        //font.draw(spriteBatch, "InitialRow, LastRow: " + tileMapRenderer.getInitialRow() + "," + tileMapRenderer.getLastRow(), 20,
-//         //       60);
-//        
-//        tmp.set(0, 0, 0);
-//        cam.unproject(tmp);
-//        //font.draw(spriteBatch, "Location: " + tmp.x + "," + tmp.y, 20, 80);
-//        spriteBatch.end();
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void resize(int arg0, int arg1) {
-//	}
-//
-//	@Override
-//	public void resume() {
-//	}
-//
-//}
