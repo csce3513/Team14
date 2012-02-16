88809cb51419f0a957bbf2e1b3b29aa9db094f7c
Jeremy Barr <jxb040@uark.edu>
2012-02-15 21:36:33 -0600

    Test input working for UP key which is jump key atm. Reads int value per gdx for up key from file to act as user input and asserts to actual gdx.key value.

--- razorback/src/com/team14/tests/TestInput.java
+++ razorback/src/com/team14/tests/TestInput.java
@@ -2,21 +2,33 @@ package com.team14.tests;
 
 import static org.junit.Assert.*;
 import java.io.*;
+import java.util.Scanner;
+import junit.framework.TestCase;
 import org.junit.Test;
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 
-public class TestInput {
 
-	@Test
-	public void test() {
-		byte [] buff ={62};
-		BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(buff));
-		System.setIn(in);
-		boolean b=true;
-		//b=Gdx.input.isKeyPressed(Keys.SPACE);
-		assertTrue(b);
+
+public class TestInput extends TestCase{
+
+    @Test                
+    public void test ()throws IOException{
+	    System.out.println("here");
+
+    	Scanner input=null;
+    	input =new Scanner(new BufferedReader(new FileReader("testinput.txt")));
+
+	// Reads input from file to mock user input and dump it to the console.
+	int	tmp	= 0;
+	while (input.hasNextInt()){
+	
+		tmp =input.nextInt();
+	    assertSame(Keys.UP,tmp);
+	    System.out.println("here");
 	}
+	// Close the streams.
 
+	  input.close();
+
+    }
 }
