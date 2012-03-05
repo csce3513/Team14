7018a4f5039523ca2f8d8cc4a17a00ec9787e4b9
Nathan Hyatt <nate@impulse.local>
2012-03-05 12:48:48 -0600

    Deleting the cruft. Deleting the cruft.

--- razorback-desktop/src/com/team14/DesktopGame.java
+++
@@ -1,9 +0,0 @@
-package com.team14;
-
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-
-public class DesktopGame {
-        public static void main (String[] args) {
-                new LwjglApplication(new Game(), "Game", 800, 600, false);
-        }
-}
